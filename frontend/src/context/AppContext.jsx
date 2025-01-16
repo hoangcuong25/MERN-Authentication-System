43a4d3e5c0bc2beabda17d3560827ea70d6e636c
hoang cuong <vancuong25112003@gmail.com>
2025-01-16 22:46:36 +0700

    fk

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext()
 
@@ -10,12 +10,27 @@ export const AppContextProvider = (props) => {
     const [isLogin, setIsLogin] = useState(false)
     const [userData, setUserData] = useState(false)
 
+    const getAuthState = async () => {
+        try {
+            const { data } = await axios.get(backenUrl + '/api/auth/is-auth')
+
+            if (data.success) {
+                setIsLogin(true)
+                getUserData()
+            }
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+
     const getUserData = async () => {
         try {
             const { data } = await axios.get(backenUrl + "/api/user/data")
             data.success ? setUserData(data.userData) : alert('error')
-        } catch {
-            console.log('error')
+        } catch (error) {
+            console.log(error.message)
         }
     }
 
@@ -25,6 +40,10 @@ export const AppContextProvider = (props) => {
         userData, setUserData,
         getUserData
     }
+
+    useEffect(() => {
+        getAuthState()
+    }, [])
 
     return (
         <AppContext.Provider value={value}>
--- frontend/src/pages/EmailVerify.jsx
+++ frontend/src/pages/EmailVerify.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const EmailVerify = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>EmailVerify</div>
+        <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+            <img onClick={() => navigate('/')} src={assets.logo} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' alt="" />
+            <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+                <h1>Email Verify OTP</h1>
+                <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email address</p>
+            </form>
+        </div>
     )
 }
 
